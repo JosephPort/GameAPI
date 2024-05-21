e8c637cfc4153526d6b16c3c8dedcfc83bd80dca
Joseph Porter <JPorterSWE@outlook.com>
2024-05-21 01:34:31 -0400

    Test

--- obj/Debug/net8.0/GameAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d24ef03256b8bf1a67817a499da9a309e0c20b9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
